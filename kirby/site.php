cd1152faea1329a647cf2cee9519a5bf8cf0fb29
Lukas Angerer <lukas.angerer@gmail.com>
2017-08-29 14:12:12 +0200

    Removed file lanugage workaround plugin

--- kirby/site.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// load the main workaround entry point file
-require(__DIR__ . DS . 'site' . DS . 'plugins' . DS . 'file-language-workaround' . DS . 'file-language-workaround.php');
-// instantiate the customized Kirby object
-$kirby = kirbyWorkaround(__DIR__);
